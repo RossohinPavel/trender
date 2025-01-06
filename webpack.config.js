d1108c1457ae61e24408e016ed8a91cd7eb002e1
HomeComputer <rossohinpavel@yandex.ru>
2025-01-06 16:18:50 +0300

    step into typescript

--- webpack.config.js
+++ webpack.config.js
@@ -6,29 +6,41 @@ module.exports = [
     {
         mode: 'production',
         entry: {
-            main: path.resolve(__dirname, './src/index.js'),
+            trender: path.resolve(__dirname, './src/index.ts'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
         },
         output: {
             path: path.resolve(__dirname, './dist'),
-            filename: 'trender.js',
+            filename: '[name].js',
             iife: false
         },
         optimization: {
             minimize: false,
         }
     },
-    {
-        mode: 'production',
-        entry: {
-            main: path.resolve(__dirname, './src/index.js'),
-        },
-        output: {
-            path: path.resolve(__dirname, './dist'),
-            filename: 'trender.min.js',
-            iife: false
-        },
-        optimization: {
-            minimize: true,
-        }
-    },
+    // {
+    //     mode: 'production',
+    //     entry: {
+    //         main: path.resolve(__dirname, './src/index.js'),
+    //     },
+    //     output: {
+    //         path: path.resolve(__dirname, './dist'),
+    //         filename: 'trender.min.js',
+    //         iife: false
+    //     },
+    //     optimization: {
+    //         minimize: true,
+    //     }
+    // },
 ]
